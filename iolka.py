64e85ce7846d9e0a8410f7187e1624cd2781644f
mako79 <mako79@bk.ru>
2021-11-28 18:12:03 +0200

    Add files via upload

---
+++ iolka.py
@@ -0,0 +1,185 @@
+import turtle as t
+t.speed(40)
+t.up()
+t.goto(0,250)
+t.down()
+t.begin_fill()# заливка начало
+t.color("yellow","yellow")# объявляем цвет звезд
+for i in range(5):# цикл от 0 до 4
+    t.forward(50)# линия 20
+    t.right(144)# поворот на 144 град
+t.end_fill()#заливка конец
+t.up()
+t.goto(25,230)
+t.down()
+t.begin_fill()# заливка начало
+t.color("green","green")# объявляем цвет звезд
+t.right(45)
+t.forward(50)
+t.right(135)
+t.forward(25)
+t.left(135)
+t.forward(60)
+t.right(135)
+t.forward(30)
+t.left(135)
+t.forward(80)
+t.right(135)
+t.forward(40)
+t.left(135)
+t.forward(80)
+t.right(135)
+t.forward(40)
+t.up()
+t.goto(25,230)
+t.down()
+t.right(-45)
+t.forward(50)
+t.right(-135)
+t.forward(25)
+t.left(-135)
+t.forward(60)
+t.right(-135)
+t.forward(30)
+t.left(-135)
+t.forward(80)
+t.right(-135)
+t.forward(40)
+t.left(-135)
+t.forward(80)
+t.right(-135)
+t.forward(152)
+t.end_fill()
+t.up()
+t.goto(15,40)
+t.down()
+t.begin_fill()# заливка начало
+t.color("brown","brown")# объявляем цвет звезд
+t.goto(30,40)
+t.goto(30,10)
+t.goto(15,10)
+t.goto(15,40)
+t.end_fill()
+t.up()
+t.goto(20,200)
+t.down()
+t.begin_fill()# заливка начало
+t.color("red","red")# объявляем цвет звезд
+t.circle(5)
+t.end_fill()
+t.up()
+t.goto(40,160)
+t.down()
+t.begin_fill()# заливка начало
+t.color("yellow","yellow")# объявляем цвет звезд
+t.circle(7)
+t.end_fill()
+t.up()
+t.goto(0,120)
+t.down()
+t.begin_fill()# заливка начало
+t.color("#336699","#223366")# объявляем цвет звезд
+t.circle(10)
+t.end_fill()
+t.up()
+t.goto(50,100)
+t.down()
+t.begin_fill()# заливка начало
+t.color("#663399","#339966")# объявляем цвет звезд
+t.circle(7)
+t.end_fill()
+t.up()
+t.goto(-10,70)
+t.down()
+t.begin_fill()# заливка начало
+t.color("#ffff00","#aabb00")# объявляем цвет звезд
+t.circle(12)
+t.end_fill()
+t.up()
+t.goto(70,60)
+t.down()
+t.begin_fill()# заливка начало
+t.color("#00ff00","#aabbaa")# объявляем цвет звезд
+t.circle(15)
+t.end_fill()
+t.up()
+t.goto(-150,0)
+t.down()
+t.begin_fill()# заливка начало
+t.color("#aaccff","#bbccff")# объявляем цвет звезд
+t.circle(25)
+t.end_fill()
+t.up()
+t.goto(-150,46)
+t.down()
+t.begin_fill()# заливка начало
+t.color("#aaccff","#bbccff")# объявляем цвет звезд
+t.circle(15)
+t.end_fill()
+t.up()
+t.goto(-150,75)
+t.down()
+t.begin_fill()# заливка начало
+t.color("#aaccff","#bbccff")# объявляем цвет звезд
+t.circle(10)
+t.end_fill()
+t.up()
+t.goto(-147,87)
+t.down()
+t.begin_fill()# заливка начало
+t.color("#121212","#fefefe")# объявляем цвет звезд
+t.circle(2)
+t.end_fill()
+t.up()
+t.goto(-156,87)
+t.down()
+t.begin_fill()# заливка начало
+t.color("#121212","#fefefe")# объявляем цвет звезд
+t.circle(2)
+t.end_fill()
+t.up()
+t.goto(-150,85)
+t.down()
+t.begin_fill()# заливка начало
+t.color("dark orange","orange")# объявляем цвет звезд
+t.goto(-165,81)
+t.goto(-150,81)
+t.goto(-150,85)
+t.end_fill()
+t.up()
+t.goto(-165,64)
+t.down()
+t.begin_fill()# заливка начало
+t.color("brown","brown")# объявляем цвет звезд
+t.goto(-182,61)
+t.end_fill()
+t.up()
+t.goto(-135,64)
+t.down()
+t.begin_fill()# заливка начало
+t.color("brown","brown")# объявляем цвет звезд
+t.goto(-120,61)
+t.end_fill()
+t.up()
+t.goto(-150,65)
+t.down()
+t.begin_fill()# заливка начало
+t.color("#121212","#222222")# объявляем цвет звезд
+t.circle(2)
+t.end_fill()
+t.up()
+t.goto(-150,50)
+t.down()
+t.begin_fill()# заливка начало
+t.color("#121212","#222222")# объявляем цвет звезд
+t.circle(2)
+t.end_fill()
+t.up()
+t.goto(-150,35)
+t.down()
+t.begin_fill()# заливка начало
+t.color("#121212","#222222")# объявляем цвет звезд
+t.circle(2)
+t.end_fill()
+t.up()
+t.goto(-150,0)
